a661b4bf53000c18332804bcbf069675fc25c3f2
Javan-Odhiambo <javan.o.otieno@gmail.com>
2023-08-25 01:03:28 +0300

    Update UI

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,15 @@
 const toggler = (e) => {
 
     let current = e.target;
-    console.log(current);
     let sibling = current.nextElementSibling || current.previousElementSibling;
     let currentSection = document.querySelector(current.dataset.section);
     let siblingtSection = document.querySelector(sibling.dataset.section);
 
     current.classList.add("active_tab");
-    current.classList.remove("hover:bg-indigo-100");
-
     sibling.classList.remove("active_tab");
-    sibling.classList.add("hover:bg-indigo-100");
 
     currentSection.classList.remove("hidden");
     siblingtSection.classList.add("hidden");
-
 }
 
 const profile = document.querySelector("#profile");
@@ -25,3 +20,4 @@ if (profile) {
         extras.classList.toggle("hidden");
     })
 }
+ 
